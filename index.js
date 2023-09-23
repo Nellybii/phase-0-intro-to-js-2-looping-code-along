4797ead67c687716e06545c1ca57850e794cf255
Nelly Chepkemoi <nellychepkem2015@gmail.com>
2023-09-23 05:19:38 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+//const names = ['james', 'Ruth', 'peter']
+function writeCards(names, event) {
+    let thankYouMessage = [];
+    for (let a = 0; a < names.length; a++) {
+        thankYouMessage.push(`Thank you, ${names[a]}, for the wonderful ${event} gift!`);
+    }
+return thankYouMessage;
+}
+function countDown() {
+    let countDown = 10;
+    while (countDown > -1) {
+        console.log(countDown--);
+    }
+}
